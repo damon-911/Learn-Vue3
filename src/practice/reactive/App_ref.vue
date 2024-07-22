4a2767a3cbee2fb34fa331ce975f8bedcd1de8de
김민준 <damon911@miksystem.net>
2024-07-22 13:19:24 +0900

    Vue3 Composition API - Reactive

---
+++ src/practice/reactive/App_ref.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+		<p>{{ message }}</p>
+		<button v-on:click="addDots">Click</button>
+	</div>
+</template>
+
+<script>
+import { reactive, ref } from 'vue';
+
+export default {
+	setup() {
+		const count = ref(0);
+		const state = reactive({
+			count,
+		});
+		console.log(count.value);
+		console.log(state.count);
+
+		const text = ref('hello');
+		const arr = reactive([text]);
+		console.log(text.value);
+		console.log(arr[0].value);
+
+		const message = ref('Message');
+		const addDots = () => {
+			message.value += '.';
+		};
+
+		return {
+			message,
+			addDots,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/practice/reactive/App_toRefs_toRef.vue
@@ -0,0 +1,30 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+import { reactive, toRefs, toRef } from 'vue';
+
+export default {
+	setup() {
+		const book = reactive({
+			author: 'Vue Team',
+			year: '2020',
+			title: 'Vue 3 Guide',
+			description: '당신은 이 책을 지금 바로 읽습니다 ;)',
+			price: '무료',
+		});
+
+		const { author, title } = toRefs(book);
+		const price = toRef(book, 'price');
+
+		return {
+			author,
+			title,
+			price,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
